3e21076a990e8a626b41f32a3a9b394ac3a88fc5
Belashov Egor <Uclaptrap@gmail.com>
2024-03-06 13:07:51 +0300

    o

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from src import DFT, EulerScheme
 
 if __name__ == '__main__':
     print('Numericals')
-    # DFT.discrete_fourier_transformation_test('files\\58.txt')
-    EulerScheme.test_euler()
+    DFT.discrete_fourier_transformation_test('files\\58.txt')
+    # EulerScheme.test_euler()
--- src/EulerScheme.py
+++ src/EulerScheme.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import matplotlib.pyplot
 from belashovplot import TiledPlot
 
 import numpy
@@ -22,7 +23,7 @@ class EulerI(Iterable):
         self._filled[0] = True
     _counter:int
     def __iter__(self) -> EulerI:
-        self._counter = -1
+        self._counter = 0
         return self
     def _sub_next(self):
         self._counter += 1
@@ -49,7 +50,7 @@ class EulerII(EulerI):
         super().__init__(t0, N, function, u0)
         self._values[1] = u1
     def __iter__(self):
-        self._counter = 0
+        self._counter = 1
         return self
     def _step(self):
         self._values[self._counter] = self._values[self._counter-2] + 2*self._dt * self._function(self._values[self._counter-1], self._counter*self._dt)
@@ -85,8 +86,13 @@ class Differential:
         return IteratorSolutionI(solver_)
     def solveII(self, t0:float, N:int, u1:numpy.ndarray=None):
         if u1 is None:
-            u1 = next(iter(self.solveI(t0, 2*N)))
-        solver_ = EulerII(t0, 2*N, self._euler_function, self._initials, u1)
+            u1 = next(iter(self.solveI(t0, N)))
+        if isinstance(u1, int):
+            for i, values in enumerate(self.solveI(t0, u1*N)):
+                if i + 1 == u1:
+                    u1 = values
+                    break
+        solver_ = EulerII(t0, N, self._euler_function, self._initials, u1)
         class IteratorSolutionII(Iterable):
             solver: EulerII
             def __init__(self, solver: EulerII):
@@ -96,17 +102,16 @@ class Differential:
                 return self
             def __next__(self):
                 value = next(self.solver)
-                next(self.solver)
                 return value
             def array(self):
-                return self.solver.array()[:][::2]
+                return self.solver.array()[:]
             def result(self):
-                return self.solver.array()[0][::2]
+                return self.solver.array()[0]
         return IteratorSolutionII(solver_)
 
 def test_euler():
     t0 = 20.0
-    N = int(2*t0)
+    N = int(t0)
     t_array = numpy.linspace(0, t0, N)
     u_array = 3.13 * numpy.cos(t_array)
 
@@ -125,9 +130,9 @@ def test_euler():
 
     system = Differential(function, initials)
     solution_methods = {
-        "без перешагивания"                                 : lambda M: system.solveI(t0, M),
-        "с перешагиванием (вторая точка без перешагивания)" : lambda M: system.solveII(t0, M),
-        "с перешагиванием (вторая точка из решения)"        : lambda M: system.solveII(t0, M, numpy.array([3.13*numpy.cos(t0/M/2), -3.13*numpy.sin(t0/M/2)]))
+        "с перешагиванием (вторая точка из решения)"        : lambda M: system.solveII(t0, M, numpy.array([3.13*numpy.cos(t0/M), -3.13*numpy.sin(t0/M)])),
+        "с перешагиванием (вторая точка из Эйлера)"         : lambda M: system.solveII(t0, M, u1=1),
+        "с перешагиванием (вторая точка из двойного Эйлера)": lambda M: system.solveII(t0, M, u1=2),
     }
     points_amount = {
         "на границе устойчивости"               : N,
@@ -135,28 +140,43 @@ def test_euler():
         "в пять раз меньше границы устойчивости": N * 5,
     }
 
-    plot = TiledPlot(12*21/9, 12)
-    plot.FontLibrary.MultiplyFontSize(0.7)
+    plot = TiledPlot(10*21/9, 10)
+    plot.FontLibrary.MultiplyFontSize(0.5)
     plot.description.bottom("t -->")
     plot.description.left("u -->")
 
     row_:int
     col_: int
-    for row_, (points_description, points_amount) in enumerate(points_amount.items()):
-        row0 = 2*row_
-        row1 = 2*row_ + 1
+    for row_, (solution_description, solution_call) in enumerate(solution_methods.items()):
+        plot.description.row.left(f'Тип решения: {solution_description}', 2*row_, 2*row_+1)
+        plot.description.row.right("Решение", 2*row_)
+        plot.description.row.right("Отклонение", 2*row_+1)
+    for col_, (points_description, points_amount) in enumerate(points_amount.items()):
         time_array = numpy.linspace(0, t0, points_amount)
         u_real = solution(time_array)
-        for col_, (solution_description, solution_call) in enumerate(solution_methods.items()):
-            col0 = 2 * col_
-            col1 = 2 * col_ + 1
+        col0 = 2 * col_
+        col1 = 2 * col_ + 1
+
+        plot.description.column.bottom(f"Количество точек: {points_amount}", col0, col1)
+        for row_, (solution_description, solution_call) in enumerate(solution_methods.items()):
+
+            row0 = 2 * row_
+            row1 = 2 * row_ + 1
             u_calculated = solution_call(points_amount).result()
 
-            axes = plot.axes.add((col0, row_), (col1, row_))
+            axes = plot.axes.add((col0, row0), (col1, row0))
             axes.grid(True)
             axes.plot(t_high_resolution, u_high_resolution, linestyle='-', color='maroon')
             axes.scatter(time_array, u_calculated, marker='.', c='green')
             axes.plot(time_array, u_calculated, linestyle='--', color='yellow')
-            plot.graph.description(f"Количество точек: {points_amount} ({points_description})\nТип схемы Эйлера: {solution_description}")
+            # plot.graph.description(f"Количество точек: {points_amount} ({points_description})\nТип схемы Эйлера: {solution_description}")
 
+            deviations = numpy.abs(u_real - u_calculated)
+
+            axes = plot.axes.add((col0, row1), (col1, row1))
+            axes.grid(True)
+            axes.plot(time_array, deviations, linestyle='--', color='blue')
+
+    plot.finalize()
+    plot._Figure.savefig('temp.svg')
     plot.show()
